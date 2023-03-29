513adcf776ad60dc64f3266d0913a726363716b6
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2023-03-29 09:33:20 +0300

    The level initializes with worm and its' body

--- src/level.py
+++ src/level.py
@@ -1,13 +1,16 @@
 import pygame
 from sprites.floor import Floor
 from sprites.wall import Wall
-
+from sprites.worm import Worm
+from sprites.body import Body
 
 class Level:
     def __init__(self, level_map, cell_size):
         self.cell_size = cell_size
         self.walls = pygame.sprite.Group()
         self.floors = pygame.sprite.Group()
+        self.worm = pygame.sprite.Group()
+        self.body = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
 
         self._initialize_sprites(level_map)
@@ -26,8 +29,14 @@ class Level:
                     self.floors.add(Floor(normalized_x, normalized_y))
                 elif cell == 1:
                     self.walls.add(Wall(normalized_x, normalized_y))
+                elif cell == 2:
+                    self.worm.add(Worm(normalized_x, normalized_y))
+                elif cell == 3:
+                    self.body.add(Body(normalized_x, normalized_y))
 
         self.all_sprites.add(
             self.floors,
             self.walls,
+            self.worm,
+            self.body
         )
--- src/sprites/body.py
+++ src/sprites/body.py
@@ -1,11 +1,12 @@
 import pygame
 from load_image import load_image
 
-class Worm(pygame.sprite.Sprite):
+class Body(pygame.sprite.Sprite):
     def __init__(self, x=0, y=0):
         super().__init__()
 
-        self.image = load_image("head.png")
+        self.image = load_image("body.png")
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
