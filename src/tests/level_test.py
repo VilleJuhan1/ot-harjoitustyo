5328142693f56e536cde2a6e8e713aca624d4dfc
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2023-03-29 13:11:40 +0300

    Made some early tests, added coverage etc.

---
+++ src/tests/level_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+from level import Level
+
+LEVEL_MAP_1 = [[1, 1, 1, 1, 1],
+               [1, 0, 0, 0, 1],
+               [1, 0, 2, 0, 1],
+               [1, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1]]
+
+CELL_SIZE = 50
+
+class TestLevel(unittest.TestCase):
+    def setUp(self):
+        self.level = Level(LEVEL_MAP_1, CELL_SIZE)
+
+    def assert_coordinates_equal(self, sprite, x, y):
+        self.assertEqual(sprite.rect.x, x)
+        self.assertEqual(sprite.rect.y, y)
+
+    def test_worm_head_is_in_the_right_starting_position(self):
+        self.assert_coordinates_equal(self.level.worm, 2 * CELL_SIZE, 2 * CELL_SIZE)
+
+    def test_worm_moves_left_by_default(self):
+        self.level._move_worm()
+        self.assert_coordinates_equal(self.level.worm, 1 * CELL_SIZE, 2 * CELL_SIZE)
+
+    def test_worm_moves_up(self):
+        self.level.worm_direction = "U"
+
+        self.level._move_worm()
+        self.assert_coordinates_equal(self.level.worm, 2 * CELL_SIZE, 1 * CELL_SIZE)
+
+
